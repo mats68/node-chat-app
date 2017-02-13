cec6396c55a656233c2f7ffcaa3c0c0793c5b6dd
Matthias <mthaler@gmx.ch>
2017-02-13 21:40:49 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+
+const app = express()
+app.use(express.static(publicPath))
+
+
+// app.get('/', (req, res) => {
+//   res.status(200).render(publicPath + 'index.html')
+// })
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}` )
+})
+
+
+module.exports = { app }
