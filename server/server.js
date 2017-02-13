a7aff149cb1f9f3ce101eb98435d066829d2a4a5
Matthias <mthaler@gmx.ch>
2017-02-13 22:14:39 +0100

    add connect event handlers

--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
 app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+  console.log('new user connected')
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected')
+  })
+})
 
 
 // app.get('/', (req, res) => {
 //   res.status(200).render(publicPath + 'index.html')
 // })
 
-app.listen(port, () => {
-  console.log(`Server started on ${port}` )
+server.listen(port, () => {
+  console.log(`Server started on port ${port}` )
 })
 
 
